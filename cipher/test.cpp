5ac7b6b4306aded544c27eeaf427f23adfaee8c2
agent-gabena <ahnu567@yandex.ru>
2023-11-26 23:36:44 +0300

    res_1

--- cipher/test.cpp
+++ cipher/test.cpp
@@ -20,7 +20,7 @@ int main (int argc, char* argv[])
     
     if (argc != 5) {
         fprintf(stdout, "Usage   : %s <plaintext file> <encrypt file> <key> <encrypt, decrypt>\n", argv[0]);
-        fprintf(stdout, "Example : %s my_text_msg.txt my_encrypt_msg.txt 0123456789012345 1,2 \nДлинна ключа должна быть в пределах [16;32] и кратна 8 \n", argv[0]);
+        fprintf(stdout, "Example : %s my_text_msg.txt my_encrypt_msg.txt 0123456789012345 1,2 \nДлина ключа должна быть в пределах [16;32] и кратна 8 \n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
